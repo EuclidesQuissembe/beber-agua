bb96acd2eec6a06983082fd8e6f1fd763158db79
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2020-12-15 14:00:42 +0100

    :tada: Initial commit
    Início do projecto

---
+++ app/src/main/java/ao/co/euclidesquissembe/beberagua/MainActivity.java
@@ -0,0 +1,14 @@
+package ao.co.euclidesquissembe.beberagua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
